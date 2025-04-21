22938fd0fd8b7896a82acce3c1b770c5ebe138a0
utmalik <utmalik@amazon.com>
2025-04-21 23:19:42 +0530

    JS Lec 2

---
+++ Module2/JSBasics/ExecutionContext/script.js
@@ -0,0 +1,122 @@
+
+
+
+// var x=5;
+// var y = 10;
+
+
+// function add(a,b){
+//     var c = a+b;
+//     return c;
+// }
+
+// var answer = add(5,6);
+
+// var z= x+y;
+
+// console.log(z);
+
+// console.log(answer);
+
+
+
+
+
+// function grandParent(){
+
+//     function parent(){
+
+//         function child(){
+
+//             var x=5;
+//             var y=7;
+//             var z=10;
+
+//             return x+y+z;
+//         }
+
+//         return child();
+//     }
+//     return parent();
+// }
+
+// var answer = grandParent();
+
+// console.log(answer);
+
+
+
+
+// function grandParent(){
+//     var x=5;
+
+//     function parent(){
+//         var y=7;
+
+//         function child(){
+
+//             var z=10;
+
+//             return x+y+z;
+//         }
+
+//         return child();
+//     }
+//     return parent();
+// }
+
+// var answer = grandParent();
+
+// console.log(answer);
+
+
+
+
+// console.log(x);
+
+// var x = 5;
+
+
+
+// add(5,9);
+
+// function add(a,b){
+//     console.log(a+b);
+// }
+
+//Hoisting 
+// Variable are partially hoisted and Functions are fully hoisted
+
+
+
+
+
+
+
+
+// add(5,10);
+
+// function add(a,b){
+//     console.log(a+b);
+// }
+
+
+
+
+
+
+//function expressions
+
+
+// add(5,10);
+
+
+// var add = function (a,b){
+//     console.log(a+b);
+// }
+
+
+
+
+
+
---
+++ Module2/JSBasics/ThisKeyword/script.js
@@ -0,0 +1,173 @@
+
+
+// var x = 5;
+// var y =6;
+
+//this -> window object 
+
+// For Global Execution context this will be a window object.
+
+// console.log(this.x);
+
+
+
+
+
+// let user = {
+
+//     name:"Alice",
+//     greet:function(){
+//         console.log(`Hello from ${this.name}`);
+//     }
+
+// }
+
+// user.greet();
+
+
+// let cap = {
+
+//     firstName:"Steve",
+//     sayHi:function(){
+//         console.log(`Hi from ${this.firstName}`);
+//     }
+// }
+
+// cap.sayHi();
+// var saHiFn = cap.sayHi;
+// window.saHiFn();
+
+
+
+
+
+ 
+
+
+
+
+// let cap = {
+//     firstName : "Steve",
+//     sayHi : function(){
+//         console.log("53", this.firstName);
+//         const iAmInner = function(){
+//             console.log("55",this.firstName);
+//         }
+//         iAmInner();
+//     }
+// }
+// cap.sayHi();
+
+
+
+
+// function add(a,b){
+//     console.log(a+b);
+// }
+
+// const add = (a,b)=>{
+//     console.log(this);
+//     return a+b;
+// }
+
+// add(4,6);
+
+
+
+
+// let cap = {
+//     firstName : "Steve",
+//     sayHi : ()=>{
+//         console.log("hi from", this.firstName);
+//     }
+// }
+// cap.sayHi();
+
+
+//Arrow functions are lexically scoped 
+
+
+
+
+// let cap = {
+//     firstName : "Steve",
+//     sayHi : function(){
+//         console.log("53", this.firstName);
+//         const iAmInner = () => {
+//             console.log("55",this.firstName);
+//         }
+//         iAmInner();
+//     }
+// }
+// cap.sayHi();
+
+
+
+
+
+// var firstName = "Loki"
+// let cap = {
+//     firstName : "Steve",
+//     sayHi : () => {
+//         console.log("53", this.firstName);
+//         const iAmInner = () => {
+//             console.log("55", this.firstName);
+//         }
+//         iAmInner();
+//     }
+// }
+// cap.sayHi();
+
+
+
+
+
+
+// var firstName = "Loki"
+// let cap = {
+//     firstName : "Steve",
+//     sayHi : function(){
+//         console.log("53", this.firstName);
+//         const subInner = () => {
+//             console.log("54", this.firstName);
+//             const iAmInner = ()=> {
+//                 console.log("55", this.firstName);
+//             }
+//             iAmInner();
+//         }
+//         subInner();
+//     }
+// }
+// cap.sayHi();
+
+
+
+// var firstName = "Loki"
+// let cap = {
+//     firstName : "Steve",
+//     sayHi : function(){
+//         console.log("53", this.firstName);
+//         const subInner = () => {
+//             console.log("54", this.firstName);
+//             function iAmInner() {
+//                 console.log("55", this.firstName);
+//             }
+//             iAmInner();
+//         }
+//         subInner();
+//     }
+// }
+// cap.sayHi();
+
+
+
+
+
+// For Global Execution context this will be a window object.
+
+// For Execution context created with method call(calling with object), this will be that object.
+
+// For Execution context created with a function call(calling without object), this will be that window.
+
+
+
